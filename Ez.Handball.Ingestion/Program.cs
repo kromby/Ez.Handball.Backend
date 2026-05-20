13cc654e7d35f2329d99e3b1ecb562296d3e2144
Guðjón Karl Arnarson <kromby@hress.org>
2026-05-20 09:06:23 +0000

    feat: wire dependency injection in Program.cs

--- Ez.Handball.Ingestion/Program.cs
+++ Ez.Handball.Ingestion/Program.cs
@@ -1,8 +1,36 @@
+using Azure.Data.Tables;
+using Azure.Storage.Blobs;
+using Ez.Handball.Ingestion.Services;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureFunctionsWebApplication()
+    .ConfigureServices((context, services) =>
+    {
+        var config = context.Configuration;
+
+        services.AddHttpClient<IHsiApiClient, HsiApiClient>(client =>
+        {
+            client.BaseAddress = new Uri(config["HsiApiBaseUrl"] ?? "https://hsi.is");
+        });
+
+        var storageConnection = config["AzureWebJobsStorage"]
+            ?? "UseDevelopmentStorage=true";
+
+        services.AddSingleton(_ => new BlobServiceClient(storageConnection));
+        services.AddSingleton<IBlobArchiver>(sp =>
+        {
+            var blobServiceClient = sp.GetRequiredService<BlobServiceClient>();
+            var containerName = config["BlobContainerName"] ?? "raw";
+            return new BlobArchiver(blobServiceClient, containerName);
+        });
+
+        services.AddSingleton(_ => new TableServiceClient(storageConnection));
+        services.AddSingleton<ITableWriter, TableWriter>();
+    })
     .Build();
 
-host.Run();
+await host.RunAsync();
